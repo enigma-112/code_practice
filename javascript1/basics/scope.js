2e25b362f3e6c9a1f791ecaa2a174f618cf73dfe
engima-112 <rrd51510@gmail.com>
2022-01-21 10:46:05 +0530

    js

---
+++ javascript1/basics/scope.js
@@ -0,0 +1,23 @@
+// let varName = "cosmos"
+// if(true){
+//     let varName = "planet"
+//     if(true){
+//         varName = "milky-way"
+//         console.log(varName)
+//     }
+//     console.log(varName)
+// }
+
+// console.log(varName)
+
+if(true){
+    if(true){
+        name = "john"
+        console.log(name)
+    }
+}
+
+if(true){
+    let name = "abc"
+    console.log(name)
+}
