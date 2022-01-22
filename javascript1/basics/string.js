0216ac708caadb180f2a8105925ff9eec44e93f3
engima-112 <rrd51510@gmail.com>
2022-01-22 07:44:39 +0530

    js

--- javascript1/basics/string.js
+++ javascript1/basics/string.js
@@ -2,3 +2,8 @@ let city = "Chittaranjan"
 let country = "India"
 let location = city + ", " + country
 console.log(location)
+
+// template strings
+
+let place = "usa"
+console.log(`we are in ${place}`)
