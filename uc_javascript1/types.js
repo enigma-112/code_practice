b43e904478371df0c78112c61548519059ff96ee
engima-112 <rrd51510@gmail.com>
2022-01-29 04:56:40 +0530

    uc_js

---
+++ uc_javascript1/types.js
@@ -0,0 +1,13 @@
+var num = 12
+var dec = 12.79
+var bool = true;
+var str = "London"
+var obj = {}
+console.log(typeof num, typeof dec, typeof bool, typeof str, typeof(obj))
+
+//objects
+var x = {}
+var y = {"name" : "Johnny Depp"}
+var z = {name : "Johnny Depp"}
+x.name = "Johnny Depp"
+console.log(x, y, z)
